3e8a10d659efbadce796d06455575225b1970166
gitbugi <oezdemirbu@web.de>
2025-04-07 16:33:56 +0200

    Übersetzungen aktualisiert

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -11,7 +11,8 @@ export class HttpService {
 
   countys = [];
 
-  url = "assets/statistik.json"; // "https://coronavirus.m.pipedream.net/";
+  url = "assets/statistik.json"; 
+  // "https://coronavirus.m.pipedream.net/";
 
   getglobal(): Observable<any> {
     return this.http.get<any>(this.url)
